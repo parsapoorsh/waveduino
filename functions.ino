9b05912afcecb499b4b9f0cd16f9aa32d70fb478
Parsa <59599626+parsapoorsh@users.noreply.github.com>
2024-02-17 14:52:37 +0330

    init commit

---
+++ functions.ino
@@ -0,0 +1,39 @@
+char convertToLetter(unsigned long decimal) {
+  char num = decimal & 0b1111;
+  switch (num) {
+    case 0b1000: return 'D';
+    case 0b0100: return 'C';
+    case 0b0010: return 'B';
+    case 0b0001: return 'A';
+    default: return '?';  // return a default value for numbers not in the list
+  }
+}
+
+
+unsigned long convertToRemote(unsigned long decimal) {
+  if (decimal > 0b1111)
+    return decimal - (decimal & 0b1111);
+  else
+    return decimal;
+}
+
+static char* dec2binWzerofill(unsigned long Dec, unsigned int bitLength) {
+  static char bin[64];
+  unsigned int i = 0;
+
+  while (Dec > 0) {
+    bin[32 + i++] = ((Dec & 1) > 0) ? '1' : '0';
+    Dec = Dec >> 1;
+  }
+
+  for (unsigned int j = 0; j < bitLength; j++) {
+    if (j >= bitLength - i) {
+      bin[j] = bin[31 + i - (j - (bitLength - i))];
+    } else {
+      bin[j] = '0';
+    }
+  }
+  bin[bitLength] = '\0';
+
+  return bin;
+} 
---
+++ remote_wifi_sdcard.ino
@@ -0,0 +1,69 @@
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+volatile unsigned long priv_decimal = 0;
+String incomingString;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  // Serial.println();
+  Serial.println("booting");
+
+  mySwitch.enableReceive(D2);
+  mySwitch.enableTransmit(D1);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.println("ready");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (mySwitch.available()) {
+    unsigned int protocol = mySwitch.getReceivedProtocol();
+    unsigned long decimal = mySwitch.getReceivedValue();
+    unsigned int length = mySwitch.getReceivedBitlength();
+    unsigned int delay = mySwitch.getReceivedDelay();
+    char* bits = dec2binWzerofill(decimal, length);
+    char button = convertToLetter(decimal);
+    unsigned long remote = convertToRemote(decimal);
+
+    if (length <= 4)
+      goto after_receiver;
+    
+    if (priv_decimal != decimal)
+        priv_decimal = decimal;
+    
+    char output[164]; // Adjust the size as needed
+    
+    sprintf(
+      output,
+      "%lu -> %s / Protocol: %d / Remote: %lu / %dbit %s / Delay: %d / Button: %c\n", 
+      millis(), (priv_decimal == decimal) ? "Repeated" : "Received", 
+      protocol, remote, length, bits, delay, button
+    );
+
+    Serial.print(output);
+
+    mySwitch.resetAvailable();
+  }
+  after_receiver:
+
+  if (Serial.available() > 0) {
+    Serial.print("sending: ");
+    
+    incomingString = Serial.readString();
+    
+    // converting String to char array
+    // TODO: use a faster method, this is too slow
+    int str_len = incomingString.length() + 1; 
+    char char_array[str_len];
+    incomingString.toCharArray(char_array, str_len);
+
+    Serial.print(incomingString);
+    mySwitch.send(char_array);
+    Serial.println(" done");
+  }
+}
